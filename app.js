8cb256063d5014a96eb8167d8f86e26f3c957953
ohancee <rayanekaddoury@gmail.com>
2022-02-28 11:13:13 +0100

    my changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const highlightMenu = () => {
     const elem = document.querySelector('.highlight');
     const homeMenu = document.querySelector('#home-page');
     const aboutMenu = document.querySelector('#about-page');
-    const servicesMenu = document.querySelector('#services-pages');
+    const servicesMenu = document.querySelector('#services-page');
     let scrollPos = window.scrollY;
     // console.log(scrollPos);
     
@@ -25,13 +25,13 @@ const highlightMenu = () => {
         aboutMenu.classList.remove('highlight');
         return;
     } else if (window.innerWidth > 960 && scrollPos < 1400) {
-        aboutMenu.classList.add('highLight');
-        homeMenu.classList.remove('highLight');
-        servicesMenu.classList.remove('highLight');
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
         return;
     } else if (window.innerWidth > 960 && scrollPos < 2345) {
-        servicesMenu.classList.add('highLight');
-        aboutMenu.classList.remove('highLight');
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
         return;
     }
 
@@ -45,7 +45,7 @@ window.addEventListener('click', highlightMenu);
 
 // Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
-    const menuBars = document.querySelector('.is-active');
+    const menuBars = document.querySelector('.is-active')
     if (window.innerWidth <= 768 && menuBars) {
         menu.classList.toggle('is-active');
         menuLinks.classList.remove('active');
