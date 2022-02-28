19d82149434b9188651fcbff1838c47b368eb3e3
ohancee <rayanekaddoury@gmail.com>
2022-02-28 10:37:05 +0100

    my changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
-const navLogo = document.querySelector('#navbar__logo')
+const navLogo = document.querySelector('#navbar__logo');
 
 // Display Mobile Menu
 const mobileMenu = () => {
@@ -16,11 +16,11 @@ const highlightMenu = () => {
     const homeMenu = document.querySelector('#home-page');
     const aboutMenu = document.querySelector('#about-page');
     const servicesMenu = document.querySelector('#services-pages');
-    let scrollPos = windows.scrollY;
-    console.log(scrollPos);
+    let scrollPos = window.scrollY;
+    // console.log(scrollPos);
     
     // adds 'highlight' class to my menu items
-    if(window.innerWidth > 960 && scrollPos < 600) {
+    if (window.innerWidth > 960 && scrollPos < 600) {
         homeMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         return;
@@ -35,23 +35,22 @@ const highlightMenu = () => {
         return;
     }
 
-    if(elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+    if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
         elem.classList.remove('highlight');
     }
-    
-}
+};
 
 window.addEventListener('scroll', highlightMenu);
 window.addEventListener('click', highlightMenu);
 
 // Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
-    const menuBars = document.querySelector('.is-active')
-    if(window.innerWidth <= 768 && menuBars) {
-        menu.classList.toggle('is-active')
-        menuLinks.classList.remove('active')
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
     }
-}
+};
 
 menuLinks.addEventListener('click', hideMobileMenu)
 navLogo.addEventListener('click', hideMobileMenu)
